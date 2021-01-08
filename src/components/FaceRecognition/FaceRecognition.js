525bc45e1f8785771bbf9b49fe45339f7dda7179
Waelyassir <wael.yassir@gmail.com>
2021-01-08 14:43:12 +0200

    handle invalid input

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import './FaceRecognition.css'
 
-const FaceRecognition = ({ boxes, imageUrl }) => {
-    return(
-        <div className='center pa4 br3'>
-            <div className='absolute'>
-                <img id='inputImage' src={ imageUrl } alt='' width='600px' height='auto' />
-                {
-                    boxes.map(box => {
-                        return(
-                            <div className='bounding-box' style={{ top:box.topRow, 
-                                 bottom: box.botRow, left: box.leftCol, right: box.rightCol }}>
-                                 {/* <div className='bounding-box_concept'>
-                                     <span>face</span>
-                                 </div> */}
-                            </div>
-                        );
-                    })
-                }
+const FaceRecognition = ({ boxes, imageUrl, isValidInput }) => {
+    if (isValidInput) {
+        return(
+            <div className='center pa4 br3'>
+                <div className='absolute'>
+                    <img id='inputImage' src={ imageUrl } alt='' width='600px' height='auto' />
+                    {
+                        boxes.map(box => {
+                            return(
+                                <div className='bounding-box' style={{ top:box.topRow, 
+                                    bottom: box.botRow, left: box.leftCol, right: box.rightCol }}>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return(
+            <div className='f2 tc pa4 fw6'>
+                {'Enter a valid Url!'}
+            </div>
+        );
+    }
+
 }
     
 export default FaceRecognition;
